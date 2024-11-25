58a9f0d8c8e985c53d1f0fe216a3ae2d5e282857
vinic1u <pvinicius2005@hotmail.com>
2024-11-25 14:56:55 -0300

    Alterando o nome da coluna carga_horairo para carga_horaria da tabela de cursos

--- src/main/resources/db/migration/V3__create-table-cursos.sql
+++ src/main/resources/db/migration/V3__create-table-cursos.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS tb_curso (
     id int not null primary key auto_increment,
     nome varchar(100),
     codigo varchar(20),
-    carga_horario int
+    carga_horaria int
 );
