2e613a26620f38ad8517765e9266789a81b3eec1
vinic1u <pvinicius2005@hotmail.com>
2024-11-28 15:32:45 -0300

    Criando Query Personalizada para buscar nota por id de aluno

--- src/main/java/com/pedrochagas/educacional/repositories/NotaRepository.java
+++ src/main/java/com/pedrochagas/educacional/repositories/NotaRepository.java
@@ -1,8 +1,22 @@
 package com.pedrochagas.educacional.repositories;
 
 import com.pedrochagas.educacional.entities.Nota;
+import com.pedrochagas.educacional.projections.AlunoNotaProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface NotaRepository extends JpaRepository<Nota,Integer> {
 
+
+    @Query(nativeQuery = true,
+        value = "SELECT n.nota,n.data_lancamento,a.nome as aluno,d.nome as disciplina " +
+                "FROM tb_nota n " +
+                "INNER JOIN tb_matricula m ON n.matricula_id  = m.id " +
+                "INNER JOIN tb_aluno a ON m.aluno_id  = a.id " +
+                "INNER JOIN tb_disciplina d ON n.disciplina_id = d.id " +
+                "WHERE a.id = :alunoId"
+    )
+    List<AlunoNotaProjection> listarNotasPorAluno(Integer alunoId);
 }
